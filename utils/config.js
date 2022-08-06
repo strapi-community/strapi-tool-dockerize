1594fbe97f784998cbffde4c7b89c6ac8df70315
Simen Daehlin <sdaehlin@luno.com>
2022-08-06 09:01:57 +0100

    perf(config.js): added a way to use global config to set/get for more speed 🚀
    Due that we had to call functions here there and everywhere it made it harder to read so we now have
    a setConfig and a d a getter
    
    re #19

--- utils/config.js
+++ utils/config.js
@@ -1,11 +1,17 @@
 const path = require('path');
 
 const _config = {
+	url: 'https://github.com/strapi-community/strapi-tool-dockerize',
 	dockerComposeDir: path.join(__dirname, '../templates/compose'),
 	dockerfileDir: path.join(__dirname, '../templates/dockerfiles'),
-	outDir: path.join(process.cwd())
+	outDir: path.join(process.cwd()),
+	projectType: 'ts',
+	packageManager: '',
+	env: 'development',
+	npmDownloads: 0,
+	githubStars: 0
 };
 const setConfig = newConfig => Object.assign(_config, newConfig);
-const getConfig = () => _config;
+const config = _config;
 
-module.exports = { setConfig, getConfig };
+module.exports = { setConfig, config };
