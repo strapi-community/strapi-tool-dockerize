c1ff6534327825b45b1077ebabe5d54a90a808d9
Simen Daehlin <simen.dehlin@gmail.com>
2025-06-25 12:24:38 +0100

    Started work with clank

--- src/utils/generation/types.ts
+++
@@ -1,7 +0,0 @@
-import { GenerationStep, GenerationStatus } from '../../types/generation.js';
-
-export interface GenerationContext {
-  config: Record<string, any>;
-  steps: GenerationStep[];
-  updateStatus: (status: GenerationStatus | ((prev: GenerationStatus) => GenerationStatus)) => void;
-} 
