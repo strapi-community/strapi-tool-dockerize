5cc921e4619664200c06356e36b850d5066bd608
Simen Daehlin <simen.dehlin@gmail.com>
2026-02-11 09:38:40 +0000

    fix liquid template lookup for Dockerfile.prod

--- src/templates/renderer.ts
+++ src/templates/renderer.ts
@@ -7,12 +7,11 @@ const TEMPLATES_DIR = join(__dirname, "files")
 
 const engine = new Liquid({
 	root: [TEMPLATES_DIR],
-	extname: ".liquid",
 	strictFilters: true,
 	strictVariables: false,
 })
 
 export async function renderTemplate(templateName: string, context: Record<string, unknown>): Promise<string> {
-	const result = await engine.renderFile(templateName, context)
+	const result = await engine.renderFile(`${templateName}.liquid`, context)
 	return result.toString()
 }
