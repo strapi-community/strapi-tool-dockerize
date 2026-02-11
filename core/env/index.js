1050a90d5931e82588046374d3104fc5f3280a23
Simen Daehlin <simen.dehlin@gmail.com>
2026-02-11 13:50:34 +0000

    remove v1 source directories and entry point

--- core/env/index.js
+++
@@ -1,9 +0,0 @@
-const createEnv = require(`./createEnv`)
-const appendEnv = require(`./appendEnv`)
-const resetEnv = require(`./resetEnv`)
-
-module.exports = {
-	createEnv,
-	appendEnv,
-	resetEnv,
-}
